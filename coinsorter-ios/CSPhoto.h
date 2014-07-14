4d6e5b7e6833468f9a926ac9ae37c8426e2a4b16
jrunzer <jrunzer@acdsystems.com>
2014-07-14 15:05:33 -0700

    added cs photo object

---
+++ coinsorter-ios/CSPhoto.h
@@ -0,0 +1,13 @@
+//
+//  CSPhoto.h
+//  coinsorter-ios
+//
+//  Created by Jake Runzer on 7/13/14.
+//  Copyright (c) 2014 Jake Runzer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CSPhoto : NSObject
+
+@end
---
+++ coinsorter-ios/CSPhoto.m
@@ -0,0 +1,13 @@
+//
+//  CSPhoto.m
+//  coinsorter-ios
+//
+//  Created by Jake Runzer on 7/13/14.
+//  Copyright (c) 2014 Jake Runzer. All rights reserved.
+//
+
+#import "CSPhoto.h"
+
+@implementation CSPhoto
+
+@end
