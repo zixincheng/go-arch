1e885051280fe33685f5c6a2d1457b5a6f8f897b
jrunzer <jrunzer@acdsystems.com>
2015-01-07 12:29:13 -0800

    checking if there is a 'Go Angel' album now using the photo framework instead of asset library.

--- Go Angel/createDefaultAlbum.m
+++ Go Angel/createDefaultAlbum.m
@@ -10,47 +10,41 @@
 
 @implementation createDefaultAlbum
 
-- (void) createAlbum{
-    // create a new album
-    PHPhotoLibrary* photoLibrary = [PHPhotoLibrary sharedPhotoLibrary];
-    
-    __block PHObjectPlaceholder* newPHAssetCollection;
-    [photoLibrary performChanges:^{
-        PHAssetCollectionChangeRequest* createRequest = [PHAssetCollectionChangeRequest creationRequestForAssetCollectionWithTitle:SAVE_PHOTO_ALBUM];
-        newPHAssetCollection = createRequest.placeholderForCreatedAssetCollection;
-    } completionHandler:^(BOOL success, NSError *error) {
-    }];
+- (void)createAlbum {
+  // create a new album
+  PHPhotoLibrary *photoLibrary = [PHPhotoLibrary sharedPhotoLibrary];
+
+  __block PHObjectPlaceholder *newPHAssetCollection;
+  [photoLibrary performChanges:^{
+      PHAssetCollectionChangeRequest *createRequest =
+          [PHAssetCollectionChangeRequest
+              creationRequestForAssetCollectionWithTitle:SAVE_PHOTO_ALBUM];
+      newPHAssetCollection = createRequest.placeholderForCreatedAssetCollection;
+  } completionHandler:^(BOOL success, NSError *error){}];
 }
 
-- (void) setDefaultAlbum{
-    assetAlbumLibrary = [[ALAssetsLibrary alloc] init];
-    __block BOOL found = NO;
-    
-    //check album group, create Go Angel album if not found
-    ALAssetsLibraryGroupsEnumerationResultsBlock
-    assetGroupEnumerator = ^(ALAssetsGroup *group, BOOL *stop){
-        if (group) {
-            NSString *albumName = [group valueForProperty:ALAssetsGroupPropertyName];
-            if ([SAVE_PHOTO_ALBUM isEqualToString:albumName]) {
-                NSLog(@"find default album: %@", SAVE_PHOTO_ALBUM);
-                *stop = YES;
-                found = YES;
-            }
-        } else {
-            if (found)
-                return;
-            
-            NSLog(@"album not found, create the %@ album",SAVE_PHOTO_ALBUM);
-            [self createAlbum];
-        }
-    };
-    
-    [assetAlbumLibrary enumerateGroupsWithTypes:ALAssetsGroupAlbum
-                           usingBlock:assetGroupEnumerator
-                         failureBlock:^(NSError *error) {
-                             NSLog(@"album access denied");
-                         }];
-}
+- (void)setDefaultAlbum {
+
+  PHFetchOptions *albumFetchOption = [[PHFetchOptions alloc] init];
+  albumFetchOption.predicate = [NSPredicate predicateWithFormat:@"title == %@", SAVE_PHOTO_ALBUM];
+  PHFetchResult *albumResult = [PHAssetCollection
+      fetchAssetCollectionsWithType:PHAssetCollectionTypeAlbum
+                            subtype:PHAssetCollectionSubtypeAlbumRegular
+                            options:albumFetchOption];
+
+  __block BOOL found = NO;
+  [albumResult enumerateObjectsUsingBlock:^(PHAssetCollection *collection,
+                                            NSUInteger idx, BOOL *stop) {
+    found = YES;
+  }];
+  
+  if (found) {
+    NSLog(@"Default Album FOUND");
+  } else {
+    NSLog(@"Default Album NOT FOUND");
+    [self createAlbum];
+  }
+
+ }
 
 @end
-
