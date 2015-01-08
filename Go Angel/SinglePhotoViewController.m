08875046c890e6d9c7b3574e0b31adc8605132a7
jrunzer <jrunzer@acdsystems.com>
2015-01-07 16:21:53 -0800

    added share button and photo sharing capabilities to single image view

--- Go Angel/SinglePhotoViewController.m
+++ Go Angel/SinglePhotoViewController.m
@@ -14,8 +14,8 @@
 
 @implementation SinglePhotoViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
+- (id)initWithNibName:(NSString *)nibNameOrNil
+               bundle:(NSBundle *)nibBundleOrNil {
   self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
   if (self) {
     // Custom initialization
@@ -23,27 +23,52 @@
   return self;
 }
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view.
-  
+
   AppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
-  [appDelegate.mediaLoader loadFullImage:self.selectedPhoto completionHandler:^(UIImage *image) {
-    
-    dispatch_async(dispatch_get_main_queue(), ^{
-      [self.imageView setImage:image];
-    });
-  }];
+  [appDelegate.mediaLoader loadFullImage:self.selectedPhoto
+                       completionHandler:^(UIImage *image) {
+
+                           dispatch_async(dispatch_get_main_queue(), ^{
+                               [self.imageView setImage:image];
+                           });
+                       }];
 }
 
-- (void) viewDidAppear:(BOOL)animated {  
-  [self.navBar setTitle:[NSString stringWithFormat:@"%d / %d", self.selected + 1, self.photos.count]];
+- (void)viewDidAppear:(BOOL)animated {
+
+  // set nav bar title
+  [self.navBar
+      setTitle:[NSString stringWithFormat:@"%d / %d", self.selected + 1,
+                                          self.photos.count]];
+
+  // add share button to top nav bar
+  self.navBar.rightBarButtonItem = [[UIBarButtonItem alloc]
+      initWithBarButtonSystemItem:UIBarButtonSystemItemAction
+                           target:self
+                           action:@selector(shareAction)];
 }
 
-- (void
-   )didReceiveMemoryWarning
-{
+- (void)shareAction {
+  
+  UIImage *image = [self.imageView image];
+  
+  NSArray *objectsToShare = @[ image ];
+
+  UIActivityViewController *activityVC =
+      [[UIActivityViewController alloc] initWithActivityItems:objectsToShare
+                                        applicationActivities:nil];
+
+  NSArray *excludeActivities = @[ ];
+
+  activityVC.excludedActivityTypes = excludeActivities;
+
+  [self presentViewController:activityVC animated:YES completion:nil];
+}
+
+- (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
   // Dispose of any resources that can be recreated.
 }
@@ -54,8 +79,9 @@
 
 /*
  #pragma mark - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
+
+ // In a storyboard-based application, you will often want to do a little
+ preparation before navigation
  - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
  {
  // Get the new view controller using [segue destinationViewController].
