8708e874fec6e48053539d2152a8ae1445d8f4e4
jrunzer <jrunzer@acdsystems.com>
2015-01-08 17:12:59 -0800

    added location table view controller and linked to storyboard view

---
+++ Go Angel/LocationTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  LocationTableViewController.h
+//  Go Angel
+//
+//  Created by Jake Runzer on 1/8/15.
+//  Copyright (c) 2015 acdGO Software Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationTableViewController : UITableViewController
+
+@end
